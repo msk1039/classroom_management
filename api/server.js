6c87e8a3fd1def26b2170de92f95a7131b689225
Mayank kadam <mayankkadam1039@gmail.com>
2025-04-10 12:10:27 +0530

    vercel doployment

--- api/server.js
+++ api/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const apiRoutes = require('./routes/api');
+const apiRoutes = require('../routes/api');
 
 const app = express();
 const PORT = 3000;
@@ -8,32 +8,32 @@ const PORT = 3000;
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public'))); // serve static files
+app.use(express.static(path.join(__dirname, '../public'))); // serve static files
 
 // Routes
 app.use('/api', apiRoutes);
 
 // Serve HTML files
 app.get('/student-login',(req, res)=>{
-    res.sendFile(path.join(__dirname, 'views', 'studentlogin.html'));
+    res.sendFile(path.join(__dirname, '../views', 'studentlogin.html'));
   })
   app.get('/teacher-login',(req, res)=>{
-      res.sendFile(path.join(__dirname, 'views', 'teacherlogin.html'));
+      res.sendFile(path.join(__dirname, '../views', 'teacherlogin.html'));
     })
   app.get('/student/dashboard',(req, res)=>{
-      res.sendFile(path.join(__dirname, 'views', 'studentdashboard.html'));
+      res.sendFile(path.join(__dirname, '../views', 'studentdashboard.html'));
     })
   
   app.get('/teacher/dashboard',(req, res)=>{
-      res.sendFile(path.join(__dirname, 'views', 'teacherdashboard.html'));
+      res.sendFile(path.join(__dirname, '../views', 'teacherdashboard.html'));
     })
     
   app.get('/student-signup',(req, res)=>{
-      res.sendFile(path.join(__dirname, 'views', 'studentsignup.html'));
+      res.sendFile(path.join(__dirname, '../views', 'studentsignup.html'));
     })
     
   app.get('/teacher-signup',(req, res)=>{
-      res.sendFile(path.join(__dirname, 'views', 'teachersignup.html'));
+      res.sendFile(path.join(__dirname, '../views', 'teachersignup.html'));
     })
 
 
